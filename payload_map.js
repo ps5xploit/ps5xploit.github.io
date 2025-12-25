41a7a18b1c84de12f64480f39ccaadeb3a94e4e0
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-12-25 23:56:19 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -9,12 +9,12 @@ const payload_map = [
         version: '2.5 beta'
     },
 {
-        displayTitle: 'etaHEN 2.1b',
+        displayTitle: 'etaHEN 2.2b',
         description: '',
-        info: 'etaHEN 2.1b by LM',
-        fileName: 'etaHEN-2.1B.bin.gz',
+        info: 'etaHEN 2.2b by LM',
+        fileName: 'etaHEN-22B.bin.gz',
         author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-        version: '2.1 beta'
+        version: '2.2 beta'
     },
     {
         displayTitle: 'HW Info',
